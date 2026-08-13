7aa97f8bf777fc2123e5d9f69c2f64c7c83d8721
Pedro H <pha.alvarez2020@gmail.com>
2026-08-13 23:23:51 +0000

    Adicionado o exercicio que era para o Dev c

---
+++ exemplo2.c
@@ -0,0 +1,15 @@
+/* Lab 1
+  * Exemplo 2 */
+
+#include <stdio.h>
+
+int main(){
+  // imprime algumas mensagens na tela
+  printf("Mackenzie - FCI\n");
+  printf("Ciência da Computação\n");
+  printf("Algoritmos e Programação II\n");
+  printf("Turma 02P11\n");
+
+
+  return 0;
+}
